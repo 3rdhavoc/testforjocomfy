990dea654759374a3bb62d9b8a3ec742af2314dd
3rdhavoc <rolandhodavid@gmail.com>
2024-12-25 00:00:57 +0000

    first commit

--- src/app/add-students/page.tsx
+++ src/app/add-students/page.tsx
@@ -1,13 +1,16 @@
+import NameofCurrentPage from "@/components/NameOfCurrentPage";
 import React from "react";
 
 export default function AddStudents(){
     return (
-        <div> 
-            <div className="grid gap-[0.01rem]   w-full h-full"
+        <div className="h-full w-full"> 
+            <div className="grid gap-[0.01rem] w-full h-full outline"
         style={{ gridTemplateColumns: '0.28fr 0.4fr 0.32fr', gridTemplateRows:'0.1 0.9' }}
       >
-        <div className="outline h-full w-full col-span-1">First column</div>
-        <div className="outline h-full w-full col-span-1">Second column</div>
+       
+        <div className="outline h-full w-full col-span-1 ">Second column</div>
+        <div className="outline h-full w-full col-span-1">
+        <NameofCurrentPage nameOfPage="New Student"/> </div>
         <div className="outline h-full w-full col-span-1">Third Colum</div>
         </div> </div>
     )
--- src/components/NameOfCurrentPage.tsx
+++ src/components/NameOfCurrentPage.tsx
@@ -8,7 +8,7 @@ interface PageInfo {
 export default function NameofCurrentPage({nameOfPage}: PageInfo){
     return (
         <div className=' pl-[2rem]'>
-            <div className="flex space-x-2 items-start  w-[30%] h-[2.5rem]">
+            <div className="flex space-x-2 items-start  w-[50%] h-[2.5rem]">
             <button className=""></button> 
             <p className='text-[1.9rem]  h-full  pr-1 pt-0 mt-[0.4rem]'>{nameOfPage}</p>
             </div>
